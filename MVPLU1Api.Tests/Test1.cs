754e0ed1db1d8ebafb4244dbebc4cb869ccd4317
Sietse Versluis <sj.versluis1@student.avans.nl>
2025-03-12 09:15:57 +0100

    Added a test test

---
+++ MVPLU1Api.Tests/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace MVPLU1Api.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int a = 1;
+            int b = 1;
+
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
